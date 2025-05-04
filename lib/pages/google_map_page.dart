27df8a429089a0abc18160fcaba756c8091eacd5
mohamedmagdi6 <mohamedmagdii137@gmai.com>
2025-05-04 22:22:05 +0300

    Refactor main.dart to use GoogleMapPage and add GoogleMapPage implementation

---
+++ lib/pages/google_map_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  State<GoogleMapPage> createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Map')),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.42796133580664, -122.085749655962),
+          zoom: 14,
+        ),
+      ),
+    );
+  }
+}
