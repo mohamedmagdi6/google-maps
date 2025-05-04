2e8fc64c98ec592290ca417399cc3514ff3594f4
mohamedmagdi6 <mohamedmagdii137@gmai.com>
2025-05-04 22:29:04 +0300

    zoom level of camera position

---
+++ lib/notics_file.dart
@@ -0,0 +1,6 @@
+// zoom view of google map 
+// 1. zoom view of world => 0 : 3  
+// 2. zoom view of country => 4 : 6 
+// 3. zoom view of city => 10 : 12
+// 4. zoom view of  street => 13 : 17 
+// 5. zoom view of building => 18 : 20
